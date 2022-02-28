426c0859f7c22e6914743b210b6f609db29c5724
isaque <isaquemariano.santos@gmail.comgit statuscd..git config --global user.email isaquemariano.santos@gmail.com>
2022-02-28 04:34:47 -0300

    Tela de inscriçao

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import core.views
+from subscriptions.views import subscribe   
 
 urlpatterns = [
     path('', core.views.home),
+    path('inscricao/', subscribe),
     path('admin/', admin.site.urls),
 ]
