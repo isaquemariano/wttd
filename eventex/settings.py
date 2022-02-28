c2c8fd2b066f266c3880b48455aad22fc42a534b
isaque <isaquemariano.santos@gmail.comgit statuscd..git config --global user.email isaquemariano.santos@gmail.com>
2022-02-27 23:25:15 -0300

    Import project

--- eventex/settings.py
+++ eventex/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from email.policy import default
 from pathlib import Path
 from decouple import config 
 from dj_database_url import parse as dburl
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
